9e8318ec7d01526ee14c673a26592cc2824adbd6
xqb64 <x@xqb.ch>
2022-02-15 00:13:32 +0100

    fix/refactor: fix leaks by refactoring and consuming the data directly instead of using an intermediate array

--- src/main.c
+++ src/main.c
@@ -1,67 +1,7 @@
 #include <stdio.h>
-#include <dirent.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
-#include <libgen.h>
-#include <unistd.h>
-
-void sieve(char *current, struct dirent **sieved, int *sieved_sz) {
-    DIR *path = opendir(current);
-
-    if (path == NULL) {
-        return;
-    }
-
-    int c, f, d, files_sz, directories_sz, combined_sz;
-    struct dirent *file, **files, **directories;
-
-    files_sz = directories_sz = combined_sz = 8;
-
-    files = malloc(files_sz * sizeof(struct dirent *));
-    directories = malloc(directories_sz * sizeof(struct dirent *));
-    
-    f = d = 0;
-    while ((file = readdir(path)) != NULL) {
-        if (f >= files_sz) {
-            files_sz *= 2;
-            files = realloc(files, files_sz * sizeof(struct dirent *));
-        }
-        if (d >= directories_sz) {
-            directories_sz *= 2;
-            directories = realloc(directories, directories_sz * sizeof(struct dirent *));
-        }
-        if (strcmp(file->d_name, ".") == 0)
-            continue;
-        if (strcmp(file->d_name, "..") == 0)
-            continue;
-        if (file->d_type == DT_DIR)
-            directories[d++] = file;
-        else
-            files[f++] = file;
-    }
-
-    for (int i = 0; i < d; ++i) {
-        if (i > combined_sz) {
-            combined_sz *= 2;
-            sieved = realloc(sieved, combined_sz * sizeof(struct dirent *));
-        }
-        sieved[i] = directories[i];
-    }
-
-    for (int i = 0; i < f; ++i) {
-        if ((i+d) > combined_sz) {
-            combined_sz *= 2;
-            sieved = realloc(sieved, combined_sz * sizeof(struct dirent *));
-        }
-        sieved[i+d] = files[i];
-    }
-
-    *sieved_sz = f + d;
-
-    free(files);
-    free(directories);
-}
 
 void print_parent_prefix(int depth, bool is_parent_last) {
     if (is_parent_last) {
@@ -74,33 +14,43 @@ void print_parent_prefix(int depth, bool is_parent_last) {
     }
 }
 
+struct dirent *superscalar_readdir(DIR *path) {
+    while (true) {
+        struct dirent *p = readdir(path);
+        if (!p) return NULL;
+        if (strcmp(p->d_name, ".") == 0 || strcmp(p->d_name, "..") == 0) continue;
+        return p;
+    }
+}
+
 void traverse(char *current, int depth, bool is_parent_last) {
     int sieved_sz;
-    struct dirent **sieved;
+    struct dirent *file, *next;
+    DIR *path;
     
     sieved_sz = 256;
+    path = opendir(current);
+    file = superscalar_readdir(path);
+    next = superscalar_readdir(path);
 
-    sieved = malloc(sieved_sz * sizeof(struct dirent *));
-    sieve(current, sieved, &sieved_sz);
-
-    for (int i = 0; i < sieved_sz; ++i) {
-        bool is_last = i + 1 == sieved_sz;
+    for (; file; file = next, next = superscalar_readdir(path)) {
+        bool is_last = next == NULL;
         const char *filename_prefix = is_last ? "┗━" : "┣━";
         
         print_parent_prefix(depth, is_parent_last);
    
-        if (sieved[i]->d_type == DT_DIR) {
-            printf("%s %s\n", filename_prefix, sieved[i]->d_name);
+        if (file->d_type == DT_DIR) {
+            printf("%s %s\n", filename_prefix, file->d_name);
             
             char spam[PATH_MAX];
-            snprintf(spam, PATH_MAX, "%s/%s", current, sieved[i]->d_name);
+            snprintf(spam, PATH_MAX, "%s/%s", current, file->d_name);
             traverse(spam, depth+1, is_last);
         } else {
-            printf("%s %s\n", filename_prefix, sieved[i]->d_name);
+            printf("%s %s\n", filename_prefix, file->d_name);
         }
     }
 
-    free(sieved);
+    closedir(path);
 }
 
 int main(int argc, char *argv[]) {
